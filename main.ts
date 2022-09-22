d440aed0dcdbd92952e7817273d1cd6f101606a5
bethbiaalice <114154526+bethbiaalice@users.noreply.github.com>
2022-09-22 08:49:17 -0300

    Atualizar 'pxt.json, main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,1 +1,20 @@
-
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.showString("oi ")
+    basic.pause(randint(1000, 5000))
+    basic.showIcon(IconNames.Skull)
+    basic.pause(2000)
+    basic.clearScreen()
+})
+basic.showLeds(`
+    . # . # .
+    # . # . #
+    # # # . #
+    . # # . .
+    . # # . .
+    `)
+basic.forever(function () {
+	
+})
