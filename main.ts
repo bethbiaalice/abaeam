a5576351df24bf356ce2258ef545ca44162708a1
bethbiaalice <114154526+bethbiaalice@users.noreply.github.com>
2022-10-06 08:49:08 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -2,19 +2,41 @@ input.onPinPressed(TouchPin.P0, function () {
     basic.showNumber(3)
     basic.showNumber(2)
     basic.showNumber(1)
-    basic.showString("oi ")
+    basic.showString("vai começar")
     basic.pause(randint(1000, 5000))
-    basic.showIcon(IconNames.Skull)
-    basic.pause(2000)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(1000)
     basic.clearScreen()
 })
-basic.showLeds(`
-    . # . # .
-    # . # . #
-    # # # . #
-    . # # . .
-    . # # . .
-    `)
-basic.forever(function () {
-	
+input.onPinPressed(TouchPin.P2, function () {
+    if (Tocar == 0) {
+        cont2 = 0
+        basic.showLeds(`
+            . . . # #
+            . . . # #
+            . . . # #
+            . . . # #
+            . . . # #
+            `)
+        basic.pause(100)
+        basic.clearScreen()
+    }
 })
+input.onPinPressed(TouchPin.P1, function () {
+    if (Tocar == 0) {
+        cont1 += 1
+        basic.showLeds(`
+            # # . . .
+            # # . . .
+            # # . . .
+            # # . . .
+            # # . . .
+            `)
+        basic.pause(1000)
+        basic.clearScreen()
+    }
+})
+let Tocar = 0
+let cont2 = 0
+let cont1 = 0
+cont2 = 0
